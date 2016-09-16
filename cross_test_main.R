3ee263ac4d68fae43480b57668323e171730362b
merrillrudd <merrillrudd@gmail.com>
2016-09-16 14:55:59 -0700

    simulation testing with LIME OM

---
+++ cross_test_main.R
@@ -0,0 +1,132 @@
+rm(list=ls())
+
+##############################################################
+##### ------------- install packages ------------------- #####
+##############################################################
+devtools::install_github("merrillrudd/LIME", build.vignettes=TRUE, dependencies=TRUE)
+library(LIME)
+
+devtools::install_github("merrillrudd/catchMSY", build.vignettes=TRUE, dependencies=TRUE)
+library(catchMSY)
+# devtools::install_github("kaskr/TMB_contrib_R/TMBhelper", dep=TRUE)
+library(TMBhelper)
+library(foreach)
+library(doParallel)
+
+##############################################################
+##### --------------- directories -----------------------#####
+##############################################################
+
+# main_dir <- "F:\\Merrill\\Git_Projects\\CatchMSY_test"
+main_dir <- "C:\\Git_Projects\\CatchMSY_test"
+source(file.path(main_dir, "R", "test_functions.R"))
+
+## setup results directory
+sim_dir <- file.path(main_dir, "cross_test")
+## remove results (for testing)
+# unlink(sim_dir, TRUE)
+## create directory
+dir.create(sim_dir, showWarnings=FALSE)
+
+## setup directory for figures
+fig_dir <- file.path(main_dir, "figs")
+dir.create(fig_dir, showWarnings=FALSE)
+
+
+##############################################################
+##### --------- simulation-testing    ------------------ #####
+##############################################################
+
+##### --------------- testing options ------------------ #####
+
+## test across different life histories 
+lh_vec <- c("CRSNAP")#, "SIGSUT", "HAKE")
+lh <- lapply(lh_vec, function(x) choose_lh_list(species=x, selex="asymptotic", param_adjust=c("R0"), val=c(1000), start_ages=1))
+names(lh) <- lh_vec
+
+## test across different true fishing mortality patterns 
+Fdyn_set <- c("Endogenous")#, "Constant", "Ramp")
+
+## test across different true recruitment patterns
+Rdyn_set <- c("Constant")#, "Pulsed", "BH")
+
+################################################
+## Run cmsy models
+################################################
+
+## data availability scenarios -- LC currently not working, remove for now. 
+avail_set <- c("catch", "catch_index", "catch_bsurvey", "catch_LC", "catch_index_LC", "catch_bsurvey_LC", "catch_ML", "catch_index_ML", "catch_bsurvey_ML") 
+# avail_set_LC <- c("catch_LC", "catch_index_LC", "catch_bsurvey_LC")
+
+da <- list("Nyears"=20, "Nyears_comp"=20, "comp_sample"=1000) 
+
+## create combos
+cmsy_modcombos <- as.matrix(expand.grid("Model"="CMSY", "Data_avail"=avail_set, "Fdyn"=paste0("F_",Fdyn_set), "Rdyn"=paste0("R_",Rdyn_set), "LH"=paste0("LH_", lh_vec)))
+
+## transform model combinations into directories
+cmsy_dir_vec <- model_paths(modcombos=cmsy_modcombos, res_dir=sim_dir)
+
+## run iterations
+itervec <- 1:10
+
+
+##--------------------- setup parallel ----------------------
+registerDoParallel(cores=2)
+
+## ------------------ simulate data -------------------------
+
+start_datagen <- Sys.time()
+
+## create true population and generated data into directories
+foreach(loop=1:length(cmsy_dir_vec), .packages=c('LIME','catchMSY')) %dopar% generateData(modpath=cmsy_dir_vec[loop], itervec=itervec, spatial=TRUE, Fdynamics=strsplit(cmsy_modcombos[loop,"Fdyn"],"_")[[1]][2], Rdynamics=strsplit(cmsy_modcombos[loop,"Rdyn"],"_")[[1]][2], LType=1, write=TRUE, lh_list=lh, data_avail_list=da, modname=paste0(cmsy_modcombos[loop,"Model"],"_",cmsy_modcombos[loop,"Data_avail"]), rewrite=FALSE)
+
+end_datagen <- Sys.time() - start_datagen
+
+## -------------- run estimation models -----------------------
+
+## catchMSY
+start_run <- Sys.time()
+
+foreach(loop=1:length(cmsy_dir_vec), .packages=c('LIME', 'catchMSY')) %dopar% tryCatch(run_cmsy(modpath=cmsy_dir_vec[loop], itervec=itervec, lh_list=lh, data_avail=cmsy_modcombos[loop,"Data_avail"], nyears=20, rewrite=FALSE), error=function(e) print(paste0("issue with ", cmsy_dir_vec[loop])))
+
+end_run <- Sys.time() - start_run
+
+modpath=cmsy_dir_vec[loop]
+itervec=itervec
+lh_list=lh
+data_avail=cmsy_modcombos[loop,"Data_avail"]
+nyears=20
+rewrite=FALSE
+
+
+## -------------- figures  -----------------------
+compare_re(dir_vec=cmsy_dir_vec, mod_names=avail_set, Fdyn_vec=Fdyn_set, Rdyn_vec=Rdyn_set, lh_num="CRSNAP", save=FALSE, fig_name="test")
+
+dir_vec=cmsy_dir_vec
+mod_names=avail_set
+Fdyn_vec=Fdyn_set
+Rdyn_vec=Rdyn_set
+lh_num="CRSNAP"
+save=FALSE
+fig_name="test"
+
+# SPRcover <- interval_coverage(modpath_vec=mixe_dirs, param="SPR", itervec=itervec)
+# compare_fits(dir_vec=mixe_dirs, mod_names=paste0("LC", c(1,10)), Ftype="Ramp", Rtype="Pulsed", fig_name="Fits_LC", LHchoose=5, iter=7, save=TRUE, coverage=SPRcover$pcover, type="all")
+
+################################################
+## Compare to LIME method
+################################################
+
+## data availability scenarios -- LC currently not working, remove for now. 
+lc_avail_set <- c("catch_LC", "catch_index_LC", "catch_bsurvey_LC") 
+
+da <- list("Nyears"=20, "Nyears_comp"=20, "comp_sample"=1000)
+
+## create combos
+lc_modcombos <- as.matrix(expand.grid("Model"=c("LIME","CMSY"), "Data_avail"=lc_avail_set, "Fdyn"=paste0("F_",Fdyn_set), "Rdyn"=paste0("R_",Rdyn_set), "LH"=paste0("LH_", lh_vec)))
+
+## transform model combinations into directories
+lc_dir_vec <- model_paths(modcombos=lc_modcombos, res_dir=sim_dir)
+lc_cmsy_dirs <- lc_dir_vec[grep("CMSY", lc_dir_vec)]
+lc_lime_dirs <- lc_dir_vec[grep("LIME", lc_dir_vec)]
+
